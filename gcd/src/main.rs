80786e162cb31c04bd18b10f51f5c8b735fd73ac
toshinori.sato <tyahhahha@gmail.com>
2024-02-02 22:47:09 +0900

    add gcd

---
+++ gcd/src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    println!("Hello, gcd(14, 8) = {}", gcd(14, 8));
+}
+
+fn gcd(mut n: u64, mut m: u64) -> u64 {
+    assert!(n != 0 && m != 0);
+
+    while m != 0 {
+        if m < n {
+            let t = m;
+            m = n;
+            n = t;
+        }
+        m = m % n;
+    }
+    n
+}
