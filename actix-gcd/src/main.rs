3d307c34386001401fd4dd6766429e707c325ca8
toshinori.sato <tyahhahha@gmail.com>
2024-02-06 22:26:28 +0900

    update actix and first impl web server

--- actix-gcd/src/main.rs
+++ actix-gcd/src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let server = HttpServer::new(|| {
+        App::new()
+            .service(get_index)
+    });
+
+    println!("Serving on http://localhost:3000...");
+
+    server.bind("127.0.0.1:3000")?
+        .run()
+        .await
+}
+
+
+#[get("/")]
+async fn get_index() -> impl Responder {
+    HttpResponse::Ok()
+        .content_type("text/html")
+        .body(
+            r#"
+                <title>GCD Calculator</title>
+                <form action="/gcd" method="POST">
+                <input type="text" name="n" >
+                <input type="text" name="m" >
+                <button type="submit">Compute GCD</button>
+                </form>
+            "#,
+        )
 }
