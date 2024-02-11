73b36dd47e7edb051e9ad0b02b2727b2121570ae
toshinori.sato <tyahhahha@gmail.com>
2024-02-11 16:08:10 +0900

    gcd web

--- actix-gcd/src/main.rs
+++ actix-gcd/src/main.rs
@@ -1,10 +1,18 @@
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, App, HttpResponse, HttpServer, Responder, post, web};
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct GcdParameters {
+    n: u64,
+    m: u64,
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let server = HttpServer::new(|| {
         App::new()
             .service(get_index)
+            .service(post_gcd)
     });
 
     println!("Serving on http://localhost:3000...");
@@ -29,4 +37,35 @@ async fn get_index() -> impl Responder {
                 </form>
             "#,
         )
+}
+
+#[post("/gcd")]
+async fn post_gcd(form: web::Form<GcdParameters>) -> HttpResponse {
+    if form.n == 0 || form.m == 0 {
+        return HttpResponse::BadRequest()
+            .content_type("text/html")
+            .body("Computing the GCD with zero is boring");
+    }
+
+    let response = format!("The greatest common divisor os the numbers {} and {}
+                                    is <b>{}</b>\n",
+                                    form.n, form.m, gcd(form.n, form.m));
+
+        HttpResponse::Ok()
+            .content_type("text/html")
+            .body(response)
+}
+
+fn gcd(mut n: u64, mut m: u64) -> u64 {
+    assert!(n != 0 && m != 0);
+
+    while m != 0 {
+        if m < n {
+            let t = m;
+            m = n;
+            n = t;
+        }
+        m = m % n;
+    }
+    n
 }
