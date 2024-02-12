5dda106bf1c2a22395f2d42c01d874d972f152d8
toshinori.sato <tyahhahha@gmail.com>
2024-02-12 22:14:51 +0900

    add gcd function lib

--- actix-gcd/src/main.rs
+++ actix-gcd/src/main.rs
@@ -1,5 +1,6 @@
 use actix_web::{get, App, HttpResponse, HttpServer, Responder, post, web};
 use serde::Deserialize;
+use gcd_lib::gcd;
 
 #[derive(Deserialize)]
 struct GcdParameters {
@@ -54,18 +55,4 @@ async fn post_gcd(form: web::Form<GcdParameters>) -> HttpResponse {
         HttpResponse::Ok()
             .content_type("text/html")
             .body(response)
-}
-
-fn gcd(mut n: u64, mut m: u64) -> u64 {
-    assert!(n != 0 && m != 0);
-
-    while m != 0 {
-        if m < n {
-            let t = m;
-            m = n;
-            n = t;
-        }
-        m = m % n;
-    }
-    n
 }
--- gcd/src/main.rs
+++ gcd/src/main.rs
@@ -1,5 +1,6 @@
 use std::str::FromStr;
 use std::env;
+use gcd_lib::gcd;
 
 fn main() {
     let mut numbers = Vec::new();
@@ -21,25 +22,4 @@ fn main() {
     }
 
     println!("gcd = {}", d);
-}
-
-fn gcd(mut n: u64, mut m: u64) -> u64 {
-    assert!(n != 0 && m != 0);
-
-    while m != 0 {
-        if m < n {
-            let t = m;
-            m = n;
-            n = t;
-        }
-        m = m % n;
-    }
-    n
-}
-
-#[test]
-fn test_gcd() {
-    assert_eq!(gcd(15, 14), 1);
-
-    assert_eq!(gcd(2 * 3 * 5 * 11 * 17, 3 * 7 * 11 * 13 * 19), 3 * 11);
 }
