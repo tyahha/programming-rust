fe5267de39c50273fa605029bcc9a432a630e361
toshinori.sato <tyahhahha@gmail.com>
2024-02-15 20:47:52 +0900

    add parse_pair function

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -1,4 +1,5 @@
 use num::Complex;
+use std::str::FromStr;
 
 fn main() {
     println!("Hello, world!");
@@ -13,4 +14,24 @@ fn escape_time(c: Complex<f64>, limit: usize) -> Option<usize> {
         z = z * z + c;
     }
     None
+}
+
+fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T, T)> {
+    match s.find(separator) {
+        None => None,
+        Some(index) => {
+            match (T::from_str(&s[..index]), T::from_str(&s[index + 1..])) {
+                (Ok(l), Ok(r)) => Some((l, r)),
+                _ => None,
+            }
+        },
+    }
+}
+
+#[test]
+fn test_parse_pair () {
+    assert_eq!(parse_pair::<i32>("",','), None);
+    assert_eq!(parse_pair::<i32>("10,",','), None);
+    assert_eq!(parse_pair::<i32>(",10",','), None);
+    assert_eq!(parse_pair::<i32>("10,100",','), Some((10, 100)));
 }
