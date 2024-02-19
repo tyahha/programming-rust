57564a9d8a5f42b189c447dd6a8c9b5b3b2b460e
toshinori.sato <tyahhahha@gmail.com>
2024-02-19 22:09:28 +0900

    output mandelbrot image

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -3,9 +3,30 @@ use std::str::FromStr;
 use image::{ColorType, ImageEncoder};
 use image::codecs::png::PngEncoder;
 use std::fs::File;
+use std::env;
 
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 5 {
+        eprintln!("Usage: {} FILE PIXELS UPPERLEFT LOWERRIGHT", args[0]);
+        eprintln!("Example: {} mandel.png 1000x750 -1.20,0.35 -1,0.20", args[0]);
+        std::process::exit(1);
+    }
+
+    let bounds = parse_pair(&args[2], 'x')
+        .expect("error parsing image dimensions");
+    let upper_left = parse_complex(&args[3])
+        .expect("error parsing upper left corner point");
+    let lower_right = parse_complex(&args[4])
+        .expect("error parsing lower right corner point");
+
+    let mut pixels = vec![0; bounds.0 * bounds.1];
+
+    render(&mut pixels, bounds, upper_left, lower_right);
+
+    write_image(&args[1], &pixels, bounds)
+        .expect("error writing PNG image file");
 }
 
 fn escape_time(c: Complex<f64>, limit: usize) -> Option<usize> {
@@ -101,6 +122,6 @@ fn write_image(filename: &str, pixels: &[u8], bounds: (usize, usize)) -> Result<
     let output = File::create(filename)?;
 
     let encoder = PngEncoder::new(output);
-    encoder.write_image(pixels, bounds.0 as u32, bounds.1 as u32, ColorType::Rgb8)?;
+    encoder.write_image(pixels, bounds.0 as u32, bounds.1 as u32, ColorType::L8)?;
     Ok(())
 }
