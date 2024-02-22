9bf30a04faf0d49c7bc8ec66de3b7118b5fd8f40
toshinori.sato <tyahhahha@gmail.com>
2024-02-22 23:19:32 +0900

    print usage

--- quickreplace/src/main.rs
+++ quickreplace/src/main.rs
@@ -1,3 +1,19 @@
+use text_colorizer::*;
+
+#[derive(Debug)]
+struct Arguments {
+    target: String,
+    replacement: String,
+    filename: String,
+    output: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    print_usage();
+}
+
+fn print_usage() {
+    eprintln!("{} - change occurrences of one string into another",
+        "quickreplace".green());
+    eprintln!("Usage: quickreplace <target> <replacement> <INPUT> <OUTPUT>");
 }
