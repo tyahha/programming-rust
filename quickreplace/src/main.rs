b8cd8e8b70f82c3675a0525060502bf576788379
toshinori.sato <tyahhahha@gmail.com>
2024-02-22 23:26:20 +0900

    got Arguments

--- quickreplace/src/main.rs
+++ quickreplace/src/main.rs
@@ -9,11 +9,30 @@ struct Arguments {
 }
 
 fn main() {
-    print_usage();
+    let args = parse_arguments();
+    println!("{:?}", args);
 }
 
 fn print_usage() {
     eprintln!("{} - change occurrences of one string into another",
         "quickreplace".green());
     eprintln!("Usage: quickreplace <target> <replacement> <INPUT> <OUTPUT>");
+}
+
+fn parse_arguments() -> Arguments {
+    let args: Vec<String> = std::env::args().skip(1).collect();
+
+    if args.len() != 4 {
+        print_usage();
+        eprintln!("{} wrong number of arguments: expected 4, got {}.",
+            "Error: ".red(), args.len());
+        std::process::exit(1);
+    }
+
+    Arguments {
+        target: args[0].clone(),
+        replacement: args[1].clone(),
+        filename: args[2].clone(),
+        output: args[3].clone(),
+    }
 }
