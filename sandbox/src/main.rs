81158aa3af0b66a00da0cadbd92d402be7940053
toshinori.sato <tyahhahha@gmail.com>
2024-02-28 21:10:40 +0900

    add sandbox

---
+++ sandbox/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    checked_overflow();
+}
+
+fn checked_overflow() {
+    let mut i: i32 = 1;
+    loop {
+        i = i.checked_mul(10).expect("multiplication overflowed");
+    }
+}
