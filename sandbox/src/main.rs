9023108e58451fb8c046319b7123cb1d2f937236
toshinori.sato <tyahhahha@gmail.com>
2024-03-01 22:18:34 +0900

    various calc

--- sandbox/src/main.rs
+++ sandbox/src/main.rs
@@ -1,6 +1,9 @@
 fn main() {
     // checked_overflow();
     checked_calc();
+    wrapped_calc();
+    saturated_calc();
+    overflowed_calc();
 }
 
 fn checked_overflow() {
@@ -13,4 +16,19 @@ fn checked_overflow() {
 fn checked_calc() {
     println!("checked 10_u8 + 20 = {:?}", 10_u8.checked_add(30));
     println!("checked 100_u8 + 200 = {:?}", 100_u8.checked_add(200));
+}
+
+fn wrapped_calc() {
+    println!("wrapped 10_u8 + 20 = {:?}", 10_u8.wrapping_add(30));
+    println!("wrapped 100_u8 + 200 = {:?}", 100_u8.wrapping_add(200));
+}
+
+fn saturated_calc() {
+    println!("saturated 10_u8 + 20 = {:?}", 10_u8.saturating_add(30));
+    println!("saturated 100_u8 + 200 = {:?}", 100_u8.saturating_add(200));
+}
+
+fn overflowed_calc() {
+    println!("overflowed 10_u8 + 20 = {:?}", 10_u8.overflowing_add(30));
+    println!("overflowed 100_u8 + 200 = {:?}", 100_u8.overflowing_add(200));
 }
