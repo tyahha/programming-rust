8db7508d7b11977469808cb7ac37f2cc3e419164
toshinori.sato <tyahhahha@gmail.com>
2024-05-09 21:31:14 +0900

    function that returns ref

--- ref-sandbox/src/main.rs
+++ ref-sandbox/src/main.rs
@@ -6,10 +6,22 @@ fn main() {
     unsafe {
         println!("STASH value = {}", STASH);
     }
+
+    let a = [10, 1, 30, 5];
+    let s = smallest(&a);
+    println!("smallest is {}", *s);
 }
 
 fn f(p: &'static i32) {
     unsafe {
         STASH = p;
     }
+}
+
+fn smallest<'a>(v: &'a [i32]) -> &'a i32 {
+    let mut s = &v[0];
+    for r in &v[1..] {
+        if *r < * s { s = r; }
+    }
+    s
 }
